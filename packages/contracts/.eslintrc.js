3ffa9a6e163b998a670386d05718f4974930bee0
Sean Long <sean@labrys.io>
2025-07-23 00:50:44 +1000

    Update repo config and dependencies

--- packages/contracts/.eslintrc.js
+++ packages/contracts/.eslintrc.js
@@ -1,9 +1,8 @@
 module.exports = {
-  extends: "eslint-config-labrys",
-  parser: "@typescript-eslint/parser",
-  rules: {
-    "eslint-comments/disable-enable-pair": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
+  root: true,
+  extends: ['@slongdotexe/eslint-config/base'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
   },
-};
+}
--- packages/sde-bitmaps-lib/.eslintrc.js
+++ packages/sde-bitmaps-lib/.eslintrc.js
@@ -1,10 +1,19 @@
+// module.exports = {
+//   extends: "eslint-config-labrys",
+//   parser: "@typescript-eslint/parser",
+//   rules: {
+//     "eslint-comments/disable-enable-pair": "off",
+//     "no-unused-vars": "off",
+//     "@typescript-eslint/no-unused-vars": "error",
+//     "no-plusplus": "off",
+//   },
+// };
+
 module.exports = {
-  extends: "eslint-config-labrys",
-  parser: "@typescript-eslint/parser",
-  rules: {
-    "eslint-comments/disable-enable-pair": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "no-plusplus": "off",
+  root: true,
+  extends: ['@slongdotexe/eslint-config/base'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
   },
-};
+}
